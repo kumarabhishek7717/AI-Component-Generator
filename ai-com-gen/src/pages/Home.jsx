8e03220debe46a99c69f711ab923285fdb64923e
kumarabhishek7717 <abhishekkumar7717749@gmail.com>
2026-01-14 16:57:30 +0530

    done

--- ai-com-gen/src/pages/Home.jsx
+++ ai-com-gen/src/pages/Home.jsx
@@ -8,6 +8,8 @@ import { IoCopy } from "react-icons/io5";
 import { PiExportBold } from "react-icons/pi";
 import { ImNewTab } from "react-icons/im";
 import { FiRefreshCw } from "react-icons/fi";
+import { GoogleGenAI } from "@google/genai";
+import { ClipLoader } from 'react-spinners'
 
 const Home = () => {
 
@@ -19,8 +21,58 @@ const Home = () => {
     { value: 'html-tailwind-bootstrap', label: 'HTML + Tailwind + Bootstrap' },
   ];
 
-  const [outputScreen, setOutputScreen] = useState(true);
-  const [tab, setTab] = useState(1)
+  const [outputScreen, setOutputScreen] = useState(false);
+  const [tab, setTab] = useState(1);
+  const [prompt, setPrompt] = useState("");
+  const [frameWork, setFrameWork] = useState(options[0]);
+  const [code, setCode] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  function extractCode(response) {
+    // Regex se triple backticks ke andar ka content nikal lo
+    const match = response.match(/```(?:\w+)?\n?([\s\s]*?)```/);
+    return match ? match[1].trim() : response.trim();
+  }
+
+  // The client gets the API key from the environment variable `GEMINI_API_KEY`.
+  const ai = new GoogleGenAI({ apiKey: "AIzaSyBScYBfCTtImyNrAgfDk6ebBeAHqFURZz4" });
+
+  async function getResponse() {
+    setLoading(true);
+    const response = await ai.models.generateContent({
+      model: "gemini-3-flash-preview",
+      contents: `
+    You are an experienced programmer with expertise in web development and UI/UX design. You create modern, animated, and fully responsive UI components. You are highly skilled in HTML, CSS, Tailwind CSS, Bootstrap, JavaScript, React, Next.js, Vue.js, Angular, and more.
+
+Now, generate a UI component for: ${prompt}  
+Framework to use: ${frameWork.value}  
+
+Requirements:  
+The code must be clean, well-structured, and easy to understand.  
+Optimize for SEO where applicable.  
+Focus on creating a modern, animated, and responsive UI design.  
+Include high-quality hover effects, shadows, animations, colors, and typography.  
+Return ONLY the code, formatted properly in **Markdown fenced code blocks**.  
+Do NOT include explanations, text, comments, or anything else besides the code.  
+    `,
+    });
+    console.log(response.text);
+    setCode(extractCode(response.text));
+    setOutputScreen(true);
+    setLoading(false);
+
+  };
+
+  const copyCode = async () => {
+    try {
+      await navigator.clipboard.writeText(code);
+      
+    } catch (err) {
+      console.error('Failed to copy: ', err);
+    }
+
+  }
+
 
 
   return (
@@ -90,21 +142,35 @@ const Home = () => {
               indicatorSeparator: () => ({
                 display: "none",
               }),
+
+            }}
+            onChange={(e) => {
+              setFrameWork(e.value)
+
             }}
           />
           <p className='text-[15px] font-[700] mt-5'>Describe your component</p>
-          <textarea className='w-full min-h-[250px] rounded-xl bg-[#09090B] mt-3 p-[10px]' placeholder='Describe your component in detail and let ai will code for your component.'></textarea>
+          <textarea onChange={(e) => { setPrompt(e.target.value) }} value={prompt} className='w-full min-h-[250px] rounded-xl bg-[#09090B] mt-3 p-[10px]' placeholder='Describe your component in detail and let ai will code for your component.'></textarea>
           <div className='flex items-center justify-between'>
             <p className='text-[gray]'>Click on generate button to generate your code</p>
-            <button className='generate flex items-center p-[15px] rounded-lg border-0 bg-gradient-to-r from-purple-400  to-purple-600 mt-3 px-[20px] gap-[10px] transition-all hover:opacity-[.8]'><i><BsStars /></i>Generate</button>
+            <button onClick={getResponse} className='generate flex items-center p-[15px] rounded-lg border-0 bg-gradient-to-r from-purple-400  to-purple-600 mt-3 px-[20px] gap-[10px] transition-all hover:opacity-[.8]'><i><BsStars /></i>Generate</button>
           </div>
         </div>
-        <div className="right mt-2 w-[50%] h-[80vh] bg-[#141319] rounded-xl">
+        <div className="right relative mt-2 w-[50%] h-[80vh] bg-[#141319] rounded-xl">
           {
 
             outputScreen === false ?
-
               <>
+                {
+                  loading === true ?
+                    <>
+                      <div className="loader absolute left-0 top-0 w-full h-full flex items-center justify-center bg-[rgb(0,0,0,0.5)]">
+                        <ClipLoader />
+                      </div>
+
+                    </> : ""
+                }
+
                 <div className='skeleton w-full h-full flex items-center flex-col justify-center'>
                   <div className="circle p-[20px] w-[70px] flex items-center justify-center text-[30px] h-[70px] rounded-[50%] bg-gradient-to-r from-purple-400 to-purple-600"><HiCode /></div>
                   <p className='text-[16px] text-[gray] mt-3'>Your component & code will appear here.</p>
@@ -144,7 +210,7 @@ const Home = () => {
                   {
                     tab === 1 ?
                       <>
-                        <Editor height="100%" theme='vs-dark' defaultLanguage="html" Value="" />;
+                        <Editor value={code} height="100%" theme='vs-dark' defaultLanguage="html" />
                       </> :
                       <>
                         <div className='preview w-full h-full bg-white text-black flex items-center justify-center'>
